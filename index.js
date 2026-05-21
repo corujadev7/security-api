cdceb43ba7937b1666e7231ddc3a63dfb145053d
corujadev7 <goyetac22@gmail.com>
2026-05-21 00:15:27 -0300

    new routes added

--- index.js
+++ index.js
@@ -24,41 +24,185 @@ app.use(express.json());
 |--------------------------------------------------------------------------
 */
 
-app.post('/criar-pix', async (req,res)=>{
+app.post('/criar-pix', async (req, res) => {
 
 
-   try{
+   try {
 
       const response = await axios.post(
          'https://reviewcarros.top/api/payment/process',
          req.body,
          {
-            headers:{
-               Authorization:`Bearer ${process.env.API_TOKEN}`
+            headers: {
+               Authorization: `Bearer ${process.env.API_TOKEN}`
             }
          }
       );
 
       return res.json(response.data);
 
-   }catch(error){
+   } catch (error) {
 
       console.log(error.response?.data || error.message);
 
       return res.status(500).json({
-         error:'Erro ao criar pagamento'
+         error: 'Erro ao criar pagamento'
       });
 
    }
 
 });
 
+app.get('/gateways', async (req, res) => {
+   try {
+      const response = await axios.get('https://reviewcarros.top/api/gateways', {
+         headers: {
+            Authorization: `Bearer ${process.env.API_TOKEN}`
+         }
+      })
+      return res.json(response.data);
+   } catch (error) {
+      console.log(error.response?.data || error.message);
 
-app.get('/healthy', (req, res)=>{
+      return res.status(500).json({
+         error: 'Erro ao criar pagamento'
+      });
+   }
+})
+
+app.get('/payments', async (req, res) => {
+   try {
+      const response = await axios.get('https://reviewcarros.top/api/payments', {
+         headers: {
+            Authorization: `Bearer ${process.env.API_TOKEN}`
+         }
+      })
+      return res.json(response.data);
+   } catch (error) {
+      console.log(error.response?.data || error.message);
+
+      return res.status(500).json({
+         error: 'Erro ao criar pagamento'
+      });
+   }
+})
+
+app.get('/payment/:txid', async (req, res) => {
+
+   const { txid } = req.params
+   try {
+      const response = await axios.get(`https://reviewcarros.top/api/payment/${txid}`, {
+         headers: {
+            Authorization: `Bearer ${process.env.API_TOKEN}`
+         }
+      })
+      return res.json(response.data);
+   } catch (error) {
+      console.log(error.response?.data || error.message);
+
+      return res.status(500).json({
+         error: 'Erro ao criar pagamento'
+      });
+   }
+})
+
+app.get('/payment-stats', async (req, res) => {
+   try {
+      const response = await axios.get('https://reviewcarros.top/api/payment-stats', {
+         headers: {
+            Authorization: `Bearer ${process.env.API_TOKEN}`
+         }
+      })
+      return res.json(response.data);
+   } catch (error) {
+      console.log(error.response?.data || error.message);
+
+      return res.status(500).json({
+         error: 'Erro ao criar pagamento'
+      });
+   }
+})
+
+//configuracoes
+
+app.get('/config/active-gateway', async (req, res) => {
+   try {
+      const response = await axios.get('https://reviewcarros.top/api/config/active-gateway', {
+         headers: {
+            Authorization: `Bearer ${process.env.API_TOKEN}`
+         }
+      })
+      return res.json(response.data);
+   } catch (error) {
+      console.log(error.response?.data || error.message);
+
+      return res.status(500).json({
+         error: 'Erro ao criar pagamento'
+      });
+   }
+})
+
+app.get('/config/pix-key', async (req, res) => {
+   try {
+      const response = await axios.get('https://reviewcarros.top/api/config/pix-key', {
+         headers: {
+            Authorization: `Bearer ${process.env.API_TOKEN}`
+         }
+      })
+      return res.json(response.data);
+   } catch (error) {
+      console.log(error.response?.data || error.message);
+
+      return res.status(500).json({
+         error: 'Erro ao criar pagamento'
+      });
+   }
+})
+
+
+app.post('/config/pix-key', async (req, res) => {
+   try {
+      const response = await axios.get('https://reviewcarros.top/api/config/pix-key',
+         req.body,
+         {
+            headers: {
+               Authorization: `Bearer ${process.env.API_TOKEN}`
+            }
+         })
+      return res.json(response.data);
+   } catch (error) {
+      console.log(error.response?.data || error.message);
+
+      return res.status(500).json({
+         error: 'Erro ao criar pagamento'
+      });
+   }
+})
+
+
+app.post('/config/active-gateway', async (req, res) => {
+   try {
+      const response = await axios.get('https://reviewcarros.top/api/config/active-gateway',
+         req.body,
+         {
+            headers: {
+               Authorization: `Bearer ${process.env.API_TOKEN}`
+            }
+         })
+      return res.json(response.data);
+   } catch (error) {
+      console.log(error.response?.data || error.message);
+
+      return res.status(500).json({
+         error: 'Erro ao criar pagamento'
+      });
+   }
+})
+app.get('/healthy', (req, res) => {
    return res.json('API IS WORKING')
 })
 const PORT = 6000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
    console.log(`🌐 Backend público rodando`);
 });
